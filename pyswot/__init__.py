60ad67b74773489e5847a8318e39350488ac7239
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2025-08-14 09:19:01 +0200

    Export new function

--- pyswot/__init__.py
+++ pyswot/__init__.py
@@ -1,3 +1,3 @@
-from .pyswot import find_school_names, is_academic
+from .pyswot import find_school_names, is_academic, is_free
 
-__all__ = ["is_academic", "find_school_names"]
+__all__ = ["is_academic", "is_free", "find_school_names"]
